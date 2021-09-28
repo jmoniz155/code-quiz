d18ce09ea38727c499003fc45f9c5f2f3eb9068a
Jesse Moniz <jmoniz155@icloud.com>
2021-09-27 21:21:26 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # code-quiz
+
+
+## Description
+[Description](#description)
+This is where I use my knowledge in javascript/jQuery to build this Code Quiz game. It is played be answering acouple basic questions. If you take too long the timer runs out and the game is over. You can play mulitple time if you choose.
+
+
+This game works by answering acouple questions correctly before the time runs out.  Start by clicking the start button, then a timer will start to count down and you will be asked a question. When you answer the question you will be asked another question. Time will be subtracted if you answer the question incorrectly and when all questions are answered or the timer reaches zero the game is over and you will be given a score.
+
+
+[Credits]
+[Collaborators]
+[googleLinks]
+[Features]
+[Repository]
+[liveWebsite]
+
+
+## Credits
+* [Credits]
+
+
+
+## Study Material
+
+<a>mozilla:<a href="#">https://developer.mozilla.org/en-US/docs/Web/CSS/visibility
+<a>google:<a href="#">https://www.w3schools.com/tags/tag_span.asp
+<a>google:<a href="#">
+<a>google:<a href="#">
+<a>google:<a href="#">
+<a>google:<a href="#">
+<a>google:<a href="#">
+
+
+## Features
+This password generator was built with HTML 5, CSS 3 and<a>google:<a href="#"> JavaScript
+
+## Links
+
+[Repository](https://github.com/jmoniz155/password-generator)
+
+[Live Website](https://jmoniz155.github.io/password-generator/)
+
--- script.js
+++ script.js
@@ -1,1 +1,119 @@
+var startButton = document.querySelector('.start');
+var question = document.querySelector('.question');
+var answer = document.querySelector('.answer');
+var win = document.querySelector('.win');
+var lose = document.querySelector('.lose');
+var timerElement = document.querySelector('.timer-count');
+var startButton = document.querySelector('.start-button');
 
+var chosenQuestion= '';
+var numberOfQuestions= 1;
+var winCounter = 0;
+var loseCounter = 0;
+var isWin = false;
+var timer;
+var timerCount;
+
+///GIVEN I am taking a code quiz
+// Attach event listener to start button to call startGame function on click
+document.getElementsByClassName("start-button").addEventListener('click', startGame);
+
+//WHEN I click the start button
+function startGame(){
+  document.getElementsByClassName("Question-Game").style.visibility = "visible";
+}
+
+// The startGame function is called when the start button is clicked
+function startGame() {
+  isWin = false;
+  timerCount = 10;
+  // Prevents start button from being clicked when round is in progress
+  startButton.disabled = true;
+  renderBlanks();
+  startTimer();
+}
+
+//WHEN I answer a question
+
+
+//THEN I am presented with another question
+
+
+//WHEN I answer a question incorrectly
+function checkWin() {
+  // If the word equals the blankLetters array when converted to string, set isWin to true
+  if (chosenQuestion === blanksLetters.join('')) {
+    // This value is used in the timer function to test if win condition is met
+    isWin = true;
+  }
+}
+
+//THEN time is subtracted from the clock
+//THEN a timer starts and I am presented with a question
+// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+function startTimer() {
+  // Sets timer
+  timer = setInterval(function () {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (isWin && timerCount > 0) {
+      clearInterval(timer);
+      winGame();
+    }
+    // Tests if time has run out
+    if (timerCount <= 0) {
+      // Clears interval
+      clearInterval(timer);
+      loseGame();
+    }
+  }, 1000);
+}
+
+//WHEN all questions are answered or the timer reaches 0
+
+
+//THEN the game is over
+
+
+//WHEN the game is over
+// The init function is called when the page loads
+function init() {
+  getWins();
+  getlosses();
+}
+
+// The winGame function is called when the win condition is met
+function winGame() {
+  question.textContent = 'YOU WON!!!🏆 ';
+  winCounter++;
+  startButton.disabled = false;
+  setWins();
+}
+
+// The loseGame function is called when timer reaches 0
+function loseGame() {
+  question.textContent = 'GAME OVER';
+  loseCounter++;
+  startButton.disabled = false;
+  setLosses();
+}
+
+
+//THEN I can save my initials and my score
+//function to save task in local storage 
+var savedScore = function() {
+  localStorage.setItem("score", JSON.stringify(score));
+}
+var savedInit = function(initails) {
+  localStorage.setItem("initails", JSON.stringify(initails));
+}
+
+// gets tasks from local storage and load them
+function loadSaveScores() {
+  // get tasks items from local stroage
+  var savedScore = localStorage.getItem("score");
+  var savedInit = localStorage.getItem("initails");
+
+  savedScore  = JSON.parse(savedScore);
+  savedInit = JSON.parse(savedInit);
+}
